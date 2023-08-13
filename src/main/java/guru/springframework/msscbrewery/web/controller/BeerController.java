53d01931d03921e85d04a9abad64255567cc7100
kienk1803 <kien.khuattrung@ksatria.com>
2023-08-13 11:03:22 +0700

    lesson 31: Add create new beer
    - Add creating new beer endpoint.

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -3,11 +3,9 @@ package guru.springframework.msscbrewery.web.controller;
 import guru.springframework.msscbrewery.services.BeerService;
 import guru.springframework.msscbrewery.web.model.BeerDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -22,6 +20,17 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeer(@PathVariable UUID beerId) {
 
         return ResponseEntity.ok(beerService.getBeerById(beerId));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveNewBeer(@RequestBody BeerDto beerDto) {
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //todo add hostname to location uri
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+
+        return ResponseEntity.noContent().headers(headers).build();
     }
 
 }
