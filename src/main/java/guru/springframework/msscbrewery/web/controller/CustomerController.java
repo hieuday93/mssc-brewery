e46ff06f2a9f36e05772b5a0234cda0cc5f483a9
kienk1803 <kien.khuattrung@ksatria.com>
2023-08-13 11:53:42 +0700

    lesson 36_assignment2: Add save, update, delete a customer
    - Add save, update, delete a customer endpoints.
    - Add log in service methods

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -4,11 +4,10 @@ import guru.springframework.msscbrewery.services.CustomerService;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,6 +23,33 @@ public class CustomerController {
     public ResponseEntity<CustomerDto> getCustomer(@PathVariable UUID id) {
         log.info("HTTP GET /api/v1/customer/{}", id);
         return ResponseEntity.ok(customerService.getCustomerById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveNewCustomer(@RequestBody CustomerDto customerDto) {
+        log.info("HTTP POST /api/v1/customer {}", customerDto);
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //todo add hostname to location uri
+        headers.add("Location", "/api/v1/customer/" + savedDto.getId().toString());
+
+        return ResponseEntity.noContent().headers(headers).build();
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<CustomerDto> updateCustomer(@PathVariable UUID customerId, @RequestBody CustomerDto customerDto) {
+        log.info("HTTP PUT /api/v1/customer/{} {}", customerId, customerDto);
+        customerDto.setId(customerId);
+        customerService.updateCustomer(customerDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable UUID customerId) {
+        log.info("HTTP DELETE /api/v1/customer/{}", customerId);
+        customerService.deleteCustomer(customerId);
     }
 
 }
